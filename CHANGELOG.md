1a8b036fe8fc5a1e9232420091c050292b20dd38
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-12-03 16:32:21 +0100

    Add skeleton files
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# k3s CHANGELOG
+
+This file is used to list changes made in each version of the k3s cookbook.
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'k3s'
+maintainer       'Schuberg Philis'
+maintainer_email 'cookbooks@schubergphilis.com'
+license          'Apache-2.0'
+description      'Installs k3s'
+version          '0.1.0'
+chef_version     '>= 15.0'
+
+issues_url 'https://github.com/SBP-Cookbooks/k3s/issues'
+source_url 'https://github.com/SBP-Cookbooks/k3s'
